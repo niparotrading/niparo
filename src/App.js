6f50fd7a4deeb621902c096d45beb906339954c1
iamaryanyadavv <aryanyadav1601@gmail.com>
2024-03-12 23:53:26 +0530

    added dark them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
+
     return (
         <NextUIProvider>
-            <div className="App"><h1 class="text-3xl font-bold underline">
-                Hello world! hi
-            </h1> </div>
+            <div className="App dark">
+                <h1 class="text-3xl font-bold underline">
+                    Testing
+                </h1>
+            </div>
         </NextUIProvider>
     );
 }
