8db725bc8f66e26517dc7598428dcdabb300b4bb
zahaanshapoorjee <zahaan@shapoorjee.com>
2024-03-20 00:01:58 +0530

    Fixed testimonials bug

--- src/components/Htestimonials/testimonials.js
+++ src/components/Htestimonials/testimonials.js
@@ -5,6 +5,7 @@ import Test1 from '../../assets/Testimonial1.jpeg'
 import Test2 from '../../assets/Testimonial2.jpg'
 import startingQuote from '../../assets/startingQuote.png'
 import endingQuote from '../../assets/endingQuote.png'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Ensure carousel CSS is imported
 import './testimonials.css'
 
 export default function Testimonials() {
@@ -26,18 +27,10 @@ export default function Testimonials() {
 
     return (
         <>
-            <Grid.Container css={{
-                jc: 'center'
-            }}>
+            <Grid.Container css={{ jc: 'center' }}>
                 <Text css={{
-                    '@xsMin': {
-                        fontSize: '$4xl',
-                        padding: '36px 0px 24px 0px'
-                    },
-                    '@xsMax': {
-                        fontSize: '$2xl',
-                        padding: '36px 0px 24px 0px'
-                    },
+                    '@xsMin': { fontSize: '$4xl', padding: '36px 0px 24px 0px' },
+                    '@xsMax': { fontSize: '$2xl', padding: '36px 0px 24px 0px' },
                     fontWeight: '$semibold',
                     textGradient: "45deg, $purple600 20%, #CCA3FF 100%",
                     width: '100vw',
@@ -51,97 +44,45 @@ export default function Testimonials() {
                 showDots={true}
                 infiniteLoop={true}
                 autoPlay={false}
-                slidesToSlide={3}
                 stopOnHover={true}
                 swipeable={true}
                 useKeyboardArrows={true}
                 showThumbs={false}
+                centerMode={true} // Enable center mode if desired
+                centerSlidePercentage={50} // Adjust slide percentage to control size
             >
-
-                <div>
-
-                    <Grid css={{
-                        justifyContent: 'center',
-                        width: '300px'
-                    }}>
-                        <Row css={{
-                            jc: 'center',
-                            width: '300px'
-                        }}>
-                            <Col css={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center'
-                            }}>
-                                <Image src={Test1}
-                                    css={{
-                                        width: '60px',
-                                        height: '60px',
-                                        borderRadius: '30px'
-                                    }} />
-                                <Text css={{
-                                    fontSize: '$md',
-                                    fontWeight: '$semibold',
-                                    lineHeight: '1.3',
-                                    paddingTop: '6px'
-                                }}>
-                                    Aryan Yadav
+                {data.map((testimonial, index) => (
+                    <div key={index} style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Grid css={{ maxWidth: '500px' }}> {/* Adjust maxWidth as needed */}
+                            <Row justify="center" align="center">
+                                <Col css={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                    <Image src={testimonial.image}
+                                        css={{
+                                            width: '100px', // Adjust image size as needed
+                                            height: '100px',
+                                            borderRadius: '50%'
+                                        }}
+                                        alt="Testimonial"
+                                    />
+                                    <Text css={{ fontSize: '$lg', fontWeight: '$semibold', paddingTop: '12px' }}>
+                                        {testimonial.name}
+                                    </Text>
+                                    <Text css={{ fontSize: '$md', fontWeight: '$regular' }}>
+                                        {testimonial.role}
+                                    </Text>
+                                </Col>
+                            </Row>
+                            <Row justify="center" align="center">
+                                <Image src={startingQuote} alt="Start quote" css={{ width: '24px', height: '24px' }} />
+                                <Text css={{ fontSize: '$base', padding: '0 12px' }}>
+                                    {testimonial.content}
                                 </Text>
-                                <Text css={{
-                                    fontSize: '$xs',
-                                    fontWeight: '$semibold'
-                                }}>
-                                    CEO, Britannia
-                                </Text>
-                            </Col>
-                            <Image src={startingQuote}
-                                css={{
-                                    width: '24px',
-                                    height: '24px'
-                                }}
-                                width={'24px'}
-                                height={'24px'}
-                            />
-                            <Text css={{
-                                fontSize: '$base'
-                            }}>
-                                Everyone at Niparo was very helpful with our needs, responding to us immediately with any requests. Greatly appreciated!
-                            </Text>
-                            <Image src={endingQuote}
-                                css={{
-                                    width: '24px',
-                                    height: '24px'
-                                }}
-                            />
-                        </Row>
-                    </Grid>
-
-                </div>
-
-                <div>
-
-                    <Grid>
-                        <Row>
-                            <Col>
-                                <Image src={Test2}
-                                    css={{
-                                        width: '60px',
-                                        height: '60px',
-                                        borderRadius: '30px'
-                                    }} />
-                            </Col>
-                            <Image />
-                            <Text>
-
-                            </Text>
-                            <Image />
-                        </Row>
-                    </Grid>
-
-                </div>
-
-
+                                <Image src={endingQuote} alt="End quote" css={{ width: '24px', height: '24px' }} />
+                            </Row>
+                        </Grid>
+                    </div>
+                ))}
             </Carousel>
         </>
-    )
+    );
 }
